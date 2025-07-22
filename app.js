5e388efbcf84e6d7c305a241e607179043fea345
sondip dey <adrtadsx@gmail.com>
2025-07-23 01:26:10 +0600

    modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import express from 'express';
 
-app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
