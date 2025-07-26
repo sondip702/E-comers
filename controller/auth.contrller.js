6e10f0c25236acd5d9a1f9bb0b5ddd6640f3c3ec
sondip dey <adrtadsx@gmail.com>
2025-07-27 03:18:23 +0600

    modified:   controller/auth.contrller.js 	modified:   controller/auth.contrller.js

--- controller/auth.contrller.js
+++ controller/auth.contrller.js
@@ -70,7 +70,6 @@ export const login = async (req, res) => {
         });
 
     } catch (error) {   
-        console.error(error);
         res.status(500).json({ message: "Internal server error" });
     }
     
