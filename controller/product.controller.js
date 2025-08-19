5d4b0f41591063f85ff9fee31a85afcaca1bb4fc
sondip dey <adrtadsx@gmail.com>
2025-08-19 12:14:35 +0600

    modified:   controller/product.controller.js 	modified:   routes/product.route.js

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -67,6 +67,20 @@ export const updateProduct = async (req, res) => {
 }
 
 // deleteProduct(id)
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Shop.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found.' });
+        }
+        await Shop.findByIdAndDelete(id);
+        res.status(200).json({ message: 'Product deleted successfully.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+};
 
 // getAllProducts(query) (search, sort, pagination)
 
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {createProduct, updateProduct} from "../controller/product.controller.js";
+import {createProduct, deleteProduct, updateProduct} from "../controller/product.controller.js";
 import { verifyUser } from "../utility/auth.middleware.js";
 import { isAdmin } from "../utility/admin.middleware.js";
 const product = express.Router();
@@ -8,5 +8,5 @@ const product = express.Router();
 // Create a new product
 product.post("/create", verifyUser, createProduct);
 product.put("/update/:id", verifyUser,  updateProduct );
-
+product.delete("/delete/:id", verifyUser, deleteProduct);
 export default product;
