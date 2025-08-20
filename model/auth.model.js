7775953812080ef64b063091308cc8e0972fcca6
sondip dey <adrtadsx@gmail.com>
2025-08-21 01:42:07 +0600

    modified:   model/auth.model.js

--- model/auth.model.js
+++ model/auth.model.js
@@ -46,5 +46,13 @@ const authSchema = new mongoose.Schema({
 const Auth = mongoose.model("Auth", authSchema);
 
 
+authSchema.pre("save", async function(next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
 
 export default Auth;
