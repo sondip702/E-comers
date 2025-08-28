1e11bcf969997cbec5375025ab324153e5fffeae
sondip dey <adrtadsx@gmail.com>
2025-08-29 02:59:24 +0600

    modified:   routes/cart.route.js

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -3,16 +3,10 @@ import { addToCart,updateCartItem,removeCartItem, getUserCart, emptyCart } from 
 import { verifyUser } from '../utility/auth.middleware.js';
 const cart = express.Router();
 
-// Add a product to the cart
 cart.post('/add', verifyUser, addToCart);
-// Update a cart item
 cart.put('/update/:cartItemId', verifyUser, updateCartItem);
-// Remove a cart item
 cart.delete('/remove/:cartItemId', verifyUser, removeCartItem);
-// Get user's cart
 cart.get('/', verifyUser, getUserCart);
-// Empty the cart
 cart.delete('/empty', verifyUser, emptyCart);
-// Export the cart router
 
 export default cart;
