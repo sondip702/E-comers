67df91192699711b219f246d9176cbbf44c090bc
sondip dey <adrtadsx@gmail.com>
2025-09-08 14:05:20 +0600

    modified:   routes/product.route.js

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,10 +2,8 @@ import express from "express";
 
 import {createProduct, deleteProduct, updateProduct} from "../controller/product.controller.js";
 import { verifyUser } from "../utility/auth.middleware.js";
-import { isAdmin } from "../utility/admin.middleware.js";
 const product = express.Router();
 
-// Create a new product
 product.post("/create", verifyUser, createProduct);
 product.put("/update/:id", verifyUser,  updateProduct );
 product.delete("/delete/:id", verifyUser, deleteProduct);
